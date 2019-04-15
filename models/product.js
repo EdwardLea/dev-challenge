d966bcbbc9d66121793f5d9cdb0b6d4c80b26fea
edwardlea <leaedward87@gmail.com>
2019-04-15 15:06:25 +0100

    product model added to include supplier, product and price

---
+++ models/product.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  supplier: { type: String, required: 'Please give a supplier name' },
+  product: { type: String, required: 'Please give a product name' },
+  price: { type: Number, required: 'Please give a product price' }
+})
+
+
+module.exports = mongoose.model('Product', productSchema)
